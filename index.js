a8c1164d525dd9e88c3eebcf12cf89d74c6da7da
Safinpeal222 <pealmhmd222@gmail.com>
2022-05-12 23:08:43 +0600

    database added

--- index.js
+++ index.js
@@ -4,30 +4,63 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 5000;
 var cors = require("cors");
 app.use(cors());
+const ObjectId = require('mongodb').ObjectId;
 app.use(express.json());
 
 
-const uri = "mongodb+srv://book-user: PIRXtp4jL180AKyh@cluster0.h0u2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://book-user:XZTQoDSHVOxf4L5X@cluster0.h0u2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log("Databasa connected");
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Angular");
+    const haiku = database.collection("CRUD");
 
-});
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-});
-users = [];
+    //get method
+    app.get('/users',async(req,res)=>{
+      const cursor = haiku.find({});
+      const users =await cursor.toArray();
+      res.send(users);
+
+      
+  });
+
+  //Delete
+  app.delete('/users/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await  haiku.deleteOne(query);
+    console.log("deleteting an user ",result);
+    res.json(result);
+  })
+
+    // create a document to insert
+    app.post('/users',async(req,res)=>{
+             console.log("hitting the post");
+      const newUser = req.body;
+      // newUser.id = users.length;
+      
+      const result = await haiku.insertOne(newUser);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.json(result);
+      
+    })
+    
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// users = [
+//   {id : 0, name:'Shabana',email: 'Shabana@gmail.com',password:'123'},
+//   {id : 1, name:'Shaban',email: 'Shabana@gmail.com',password:'123'},
+//   {id : 2, name:'Shaba',email: 'Shabana@gmail.com',password:'123'},
+// ];
 
 
-app.post('/users',(req,res)=>{
-  console.log("hitting the post",req.body);
-  const newUser = req.body;
-  // users.push(newUser);
-  res.send(newUser);
-  
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
